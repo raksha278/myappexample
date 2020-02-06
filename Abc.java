f92aa3156ee17ebfea0cb5c141cce90e5a70a636
Raksha <raksha.m@profinch.com>
2020-02-06 10:42:45 +0530

    Initial commit

---
+++ Abc.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class swapvariables {
+    public static void main (String[] args){
+        int a,b,temp;
+        System.out.println("enter 2 values to swap");
+        Scanner sca= new Scanner(System.in);
+        a=sca.nextInt();
+        b=sca.nextInt();
+        temp=a;
+        a=b;
+        b=temp;
+        System.out.println("a="+a);
+        System.out.println("B="+b);
+
+
+    }
+}
